f2d82b8f073d3197b69631f9dbee02cfc354f4c4
Nicolas Pace <nicopace@gmail.com>
2021-01-13 02:41:09 -0300

    Changes api endpoint to match serverside changes.

--- plugins/lime-plugin-remotesupport/src/remoteSupportQueries.js
+++ plugins/lime-plugin-remotesupport/src/remoteSupportQueries.js
@@ -4,19 +4,19 @@ import { getSession, openSession, closeSession } from './remoteSupportApi'
 
 
 export function useSession() {
-	return useQuery(["lime-remotesupport", "get_session"], getSession, {
+	return useQuery(["tmate", "get_session"], getSession, {
 		refetchInterval: 10000
 	});
 }
 
 export function useOpenSession() {
 	return useMutation(openSession, {
-		onSuccess: () => queryCache.invalidateQueries(["lime-remotesupport", "get_session"])
+		onSuccess: () => queryCache.invalidateQueries(["tmate", "get_session"])
 	})
 }
 
 export function useCloseSession() {
 	return useMutation(closeSession, {
-		onSuccess: () => queryCache.invalidateQueries(["lime-remotesupport", "get_session"])
+		onSuccess: () => queryCache.invalidateQueries(["tmate", "get_session"])
 	})
 }
